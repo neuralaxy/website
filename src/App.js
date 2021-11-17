7b9f4d6089102412946abe402b244da336595b0b
AviroopNandy <aviroopnandy2001@gmail.com>
2021-11-17 18:39:54 +0530

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>NeuroStimDuino</h1>
+        </div>
+    )
+}
+
+export default App;
